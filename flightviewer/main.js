d7fd7866a326544115fb7f72f8f1476f78e40f31
Ryan Ehrlich <Ryansehrlich@gmail.com>
2017-06-06 22:03:19 -0700

    Fixed bug

--- flightviewer/main.js
+++ flightviewer/main.js
@@ -287,7 +287,7 @@ $(document).ready(function() {
 		viewer.dataSources.add(dataSource).then(function(dSource) {
 			flight = dSource._entityCollection._entities._array[0];
 
-			var aboveFlightPosition = Cesium.Ellipsoid.WGS84.cartesianToCartographic(flight);
+			var aboveFlightPosition = Cesium.Ellipsoid.WGS84.cartesianToCartographic(flight.position);
 			aboveFlightPosition.height += 10000;
 
 			aboveFlightPosition = Cesium.Ellipsoid.WGS84.cartographicToCartesian(aboveFlightPosition);
